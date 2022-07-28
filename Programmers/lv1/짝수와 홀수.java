d979733404fc856fd9ef3eb7cfdb57f3dba70a91
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-07-28 22:55:04 +0900

    Create 짝수와 홀수.java

---
+++ Programmers/lv1/짝수와 홀수.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int num) {
+String answer = "Odd";
+        if(num==0 ||num%2 == 0){
+answer = "Even";
+}
+        
+        return answer;
+    }
+}
