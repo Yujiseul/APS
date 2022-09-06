1d053d40193816c29946a4fffc68e09d5497933b
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-09-06 23:48:30 +0900

    Update 성격유형검사하기.java

--- Programmers/lv1/성격유형검사하기.java
+++ Programmers/lv1/성격유형검사하기.java
@@ -1,6 +1,16 @@
 class Solution {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
+        
+        // 점수 저장할 배열 만들기 int[8]
+        
+        // 4자리 -> 저장할 방법 구현 생각
+        
+        // 점수는 map으로 알파벳(키) 점수(value)로 저장
+        
+        // 배열 돌면서 점수 누적! -> map 으로 가능?? getOrDefalt 이용해보기
+        
+        
         return answer;
     }
 }
