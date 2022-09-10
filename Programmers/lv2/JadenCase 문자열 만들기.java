0b059cb5819456c39eb7ffae2fcd7e98537d191f
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-09-10 23:44:02 +0900

    Update JadenCase 문자열 만들기.java

--- Programmers/lv2/JadenCase 문자열 만들기.java
+++ Programmers/lv2/JadenCase 문자열 만들기.java
@@ -2,19 +2,24 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         
-        String[] arr = s.split("");
+        String[] arr = s.split(" ");
         
         for(int i=0; i<arr.length; i++){
             if(arr[i].charAt(0)<97 || arr[i].charAt(0)>122){
+                answer += arr[i];
+                answer += " ";
                 continue;
             }
-           String temp = arr[i].substring(0,1).toUpperCase();
-           arr[i].substring(0,1) = temp;
+            String temp = arr[i].substring(0,1).toUpperCase();
+            System.out.println("값" + temp);
+           answer += temp;
+            answer += arr[i].substring(1,arr[i].length()).toLowerCase();
+            answer += " ";
         }
         
-        System.out.println((int)'a');
-        System.out.println((int)'A');
-        
+        answer = answer.trim();
         return answer;
     }
 }
+
+// 실패, 
