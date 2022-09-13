b4dd6238fe671e80df6a443dfad7bf7d2606b1c0
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-09-13 20:37:32 +0900

    Create 피보나치수.java

---
+++ Programmers/lv2/피보나치수.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = fibo(n);
+        answer = num % 1234567;
+        return answer;
+    }
+    
+    public int fibo(int n){
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        if(n == 1){
+            return 1;
+        }
+        
+        return fibo(n-1) + fibo(n-2);
+    }
+}
+
+//시간초과
