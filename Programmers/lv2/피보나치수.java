66fb600f03b57325951ff7879b9204434e160a80
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-09-15 23:36:15 +0900

    Update 피보나치수.java

--- Programmers/lv2/피보나치수.java
+++ Programmers/lv2/피보나치수.java
@@ -20,6 +20,27 @@ class Solution {
     }
 }
 
-//시간초과
+//재귀 -> 시간초과
 
 For문 이용
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int[] nums = new int[n+1];
+        
+        nums[0] = 0;
+        nums[1] = 1;
+        
+        for(int i=2; i<=n; i++){
+            nums[i] = (nums[i-2]+nums[i-1])%1234567;
+        }
+        
+        answer = nums[n];
+        
+        return answer;
+    }
+    
+   
+}
