6b0375fe6d27f39e68775674cbf090619d2e5f3f
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-09-20 23:12:18 +0900

    Update 구명보트.java

--- Programmers/lv2/구명보트.java
+++ Programmers/lv2/구명보트.java
@@ -1,1 +1,28 @@
 // greedy(탐욕법)
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        
+        Arrays.sort(people);
+        
+        int idx = 0;
+        
+        for(int i=people.length-1 ; i>=0; i--){
+            if(i <idx){
+                break;
+            }else if(i == idx){
+                answer++;
+            }else{
+                if(people[i]+people[idx] <= limit){
+                idx++;
+            }
+            answer++;
+            }
+                
+        }
+        
+        return answer;
+    }
+}
