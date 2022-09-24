c3c871d803afde344f308d74d75ae210f96f9e9f
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-09-24 23:54:45 +0900

    Update 10816.java

--- BOJ/10816.java
+++ BOJ/10816.java
@@ -1,1 +1,33 @@
 //getOrDeafult 사용
+package aps;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int M = sc.nextInt(); // 전체 수 개수
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<M; i++) {
+			map.put(sc.nextInt(), map.getOrDefault(sc.nextInt(), 1)+1);
+		}
+		
+		int n = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i=0; i<n; i++) {
+			sb.append(map.get(sc.nextInt())).append(" ");
+		}
+		
+		System.out.println(sb.toString());
+		
+		
+	}
+
+}
