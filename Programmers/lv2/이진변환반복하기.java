80f8cf73dc78e91452074ff9be08015ffe3c292d
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-09-28 23:19:10 +0900

    Create 이진변환반복하기.java

---
+++ Programmers/lv2/이진변환반복하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = {};
+        
+        boolean flag = false;
+        
+        String[] temp = s.split("");
+        
+        while(flag == false){
+          String tmp = "";
+            for(String x : temp){
+               
+            }
+        }
+        
+        return answer;
+    }
+}
