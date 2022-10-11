60c333af594ff23c206c81dafc623457dfb719d4
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-10-11 23:45:30 +0900

    Update JadenCase 문자열 만들기.java

--- Programmers/lv2/JadenCase 문자열 만들기.java
+++ Programmers/lv2/JadenCase 문자열 만들기.java
@@ -7,21 +7,22 @@ class Solution {
         s = s.toLowerCase();
        int idx = 0;
         for(int i=0; i<s.length(); i++){
+            idx++;
             char a = s.charAt(i);
             
             if(a == ' ') {
                 idx = 0;
+            }else if(idx == 1){
+                if(a < '1' || a> '9'){
+                     a -= 32;
+                }
             }
             
             answer += String.valueOf(a);
         } 
         
-       
-            
-       
-        
-        
-        
         return answer;
     }
 }
+
+// test17 
