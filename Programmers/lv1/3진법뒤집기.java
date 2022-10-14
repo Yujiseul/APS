2fb69a14cf1d30230d62ba9453b80d2b4a559943
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-10-15 00:02:40 +0900

    Update 3진법뒤집기.java

--- Programmers/lv1/3진법뒤집기.java
+++ Programmers/lv1/3진법뒤집기.java
@@ -1,6 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        
+        String three = Integer.toString(n, 3);
+        String reverse = "";
+        
+        for(int i=three.length()-1; i>=0; i--){
+            reverse += String.valueOf(three.charAt(i));
+        }
+        int a = Integer.parseInt(reverse, 3);
+        answer = a;
         return answer;
     }
 }
