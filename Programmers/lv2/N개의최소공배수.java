48fb29ca817ea8100069e7e59d1c0fcbe8ae58bb
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-10-23 23:58:53 +0900

    Update N개의최소공배수.java

--- Programmers/lv2/N개의최소공배수.java
+++ Programmers/lv2/N개의최소공배수.java
@@ -5,3 +5,42 @@
 // 아니면 둘 간에 최소공배수 구하기
 
 //주의) 런타임에러
+
+import java.util.*;
+
+class Solution {
+    public int solution(int[] arr) {
+        int answer = 0;
+        
+        Arrays.sort(arr);
+        int com = arr[arr.length-1];
+        while(true){
+        for(int i = arr.length-2; i>=0; i--){
+            
+                while(true){
+                    if(com%arr[i] != 0){
+                        com++;
+                    }else{
+                        break;
+                    }
+                }
+             
+         }
+            boolean flag = true;
+            
+            for(int i=0; i<arr.length; i++){
+                if(com%arr[i] != 0){
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if(flag){
+                break;
+            }
+        }
+        answer = com;
+        
+        return answer;
+    }
+}
