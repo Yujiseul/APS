909ecf957fa9da08d0ad1c7e047ffd3b229f898a
Yujiseul <98597009+Yujiseul@users.noreply.github.com>
2022-10-25 00:01:11 +0900

    Update 멀리뛰기.java

--- Programmers/lv2/멀리뛰기.java
+++ Programmers/lv2/멀리뛰기.java
@@ -1,25 +1,20 @@
 class Solution {
-    static long ans = 0;
+  
     public long solution(int n) {
         long answer = 0;
         
-        recursion(0,n);
+        int[] arr = new int[2001];
         
-        answer = ans%1234567;
+        arr[1] = 1;
+        arr[2] = 2;
         
+        for(int i=3; i<arr.length; i++){
+            arr[i] = (arr[i-1] + arr[i-2])%1234567;;
+        }
+        
+        answer = arr[n];
         return answer;
     }
     
-    static void recursion(int s, int n){
-        if(s>n){
-            return;
-        }else if(s == n){
-            ans++;
-        }
-        
-        recursion(s+1, n);
-        recursion(s+2, n);
-    }
+    
 }
-
-// 시간초과나는 케이스 존재 
